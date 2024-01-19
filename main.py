fb26dfaa7bb5e799d36298363296e5caaf2b72f0
DrunkenPhilosoph <130548981+DrunkenPhilosoph@users.noreply.github.com>
2024-01-19 17:10:14 +0300

    Update main.py
    change main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from application.db.people import get_employees
 from application.salary import calcualte_salary
 import datetime
 
+date_now = datetime.datetime.now()
+print(date_now)
 
 if __name__ == '__main__':
     print(calcualte_salary(3))
     print(get_employees(2))
-    date_now = datetime.datetime.now()
-    print(date_now)
